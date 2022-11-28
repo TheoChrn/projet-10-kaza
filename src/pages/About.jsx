0711934e14841299cf9d26fc8f663cb737b4fb26
Theo Cheron <35193221+Volturuss@users.noreply.github.com>
2022-11-29 00:38:05 +0100

    Finished about page
    Created a Collapse component
    Style added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,42 @@
 import React from "react";
+import Collapse from "../components/Collapse";
 import Header from "../components/Header";
 
 const About = () => {
+  const collapses = [
+    {
+      value: "Fiabilité",
+      description:
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
+    },
+    {
+      value: "Respect",
+      description:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      value: "Service",
+      description:
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+      value: "Sécurité",
+      description:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ];
+
   return (
     <div className="container">
-      <h1>A propos</h1>
       <Header />
-      <main></main>
+      <main>
+        <div id="illustration"></div>
+        <section className="collapses">
+          {collapses.map((c) => (
+            <Collapse value={c.value} description={c.description} />
+          ))}
+        </section>
+      </main>
     </div>
   );
 };
